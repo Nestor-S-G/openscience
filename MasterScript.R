ada9f6b08c89232c4973de781a43b61d45b53bd9
Néstor <nsuarez163@alumno.uned.es>
2026-02-09 08:49:05 +0100

    Use callr for sourcing scripts and set working directory
    Refactor script execution to use callr for parallel processing and maintain working directory.

--- MasterScript.R
+++ MasterScript.R
@@ -9,33 +9,19 @@ renv::restore()
 #Libraries
 library(rmarkdown)
 library(here)
+library(callr)
 
 root <- here()
 
 #Script Payzan-LeNestour and Woodford (2022)
-setwd(root)
-source("Article1_Outlier/Outlier.R")
+try(
+  callr::r(function() {
+    library(here)
+    setwd(here::here())
+    source("Article1_Outlier/Outlier.R")
+  }),
+  silent = FALSE
+)
 
 #Script Payzan-LeNestour et al. (2024)
 setwd(root)
-source("Article4_Stubborn/run_full_script.R")
-
-# #Script Huber and Huber (2020)
-setwd(root)
-try(rmarkdown::render("Article5_BadBankers/notebook.Rmd"))
-
-# #Script Snijder et al. (2024)
-setwd(root)
-source("Article7_DecisionMakers/scripts/data_analysis/models.R")
-setwd(root)
-source("Article7_DecisionMakers/scripts/data_analysis/partner choice.R")
-setwd(root)
-source("Article7_DecisionMakers/scripts/data_analysis/plots.R")
-setwd(root)
-source("Article7_DecisionMakers/scripts/data_analysis/political orientation.R")
-setwd(root)
-source("Article7_DecisionMakers/scripts/process_data/process data.R")
-
-# #Script Ekström et al. (2025), (R part)
-setwd(root)
-source("Article12_MakingAPromise/MakingAPromise.R")
