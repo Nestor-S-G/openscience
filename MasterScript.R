e34760c2bc76b0a7ecbfa1eb41884d22841f7c0b
Nestor-S-G <nsuarez163@alumno.uned.es>
2026-05-21 11:56:36 +0200

    Move the funcion run_script_project() out of the MasterScript.

--- MasterScript.R
+++ MasterScript.R
@@ -11,52 +11,13 @@ renv::restore(prompt = FALSE)
 library(rmarkdown)
 library(xfun)
 
-# =============================================================================
-# Main function
-# =============================================================================
-
-run_script_project <- function(expr, log_file) {
-  
-  expr <- substitute(expr)
-  message("\nSTART: ", log_file)
-  
-  try(
-    xfun::Rscript_call(
-      function(expr, log_file) {
-        
-        options(error = quote(quit(status = 1)))
-        
-        library(here)
-        setwd(here::here())
-        
-        dir.create(dirname(log_file), recursive = TRUE, showWarnings = FALSE)
-        
-        con <- file(log_file, open = "wt")
-        sink(con)
-        sink(con, type = "message")
-        
-        on.exit({
-          sink(type = "message")
-          sink()
-          close(con)
-        }, add = TRUE)
-        
-        eval(expr, envir = new.env(parent = globalenv()))
-        
-      },
-      args = list(expr = expr, log_file = log_file)
-    ),
-    silent = FALSE
-  )
-  
-  message("END: ", log_file, "\n")
-}
+source("R/run_script_project.R")
 
 # =============================================================================
 # PAPERS TO RUN
 # =============================================================================
 
-# Payzan-LeNestour et al. (2025)
+# Payzan-LeNestour et al. (2026)
 run_script_project(
   log_file = "payzan-lenestourStubbornDesignNeurobiological/Stubborn_log.txt",
   expr = {
