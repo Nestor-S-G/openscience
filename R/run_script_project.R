8cfc84b3e5cd23df4751b4618c14f6bbf52366f0
Nestor-S-G <nsuarez163@alumno.uned.es>
2026-09-19 10:29:15 +0200

    Comment removed

--- R/run_script_project.R
+++ R/run_script_project.R
@@ -1,35 +1,35 @@
 library(xfun)
 
 run_script_project <- function(script_path, log_file) {
-  
+
   message("\n=== START: ", log_file, " ===")
-  
+
   tryCatch(
     {
       xfun::Rscript_call(
         function(script_path, log_file) {
-          
-          # Removed options(error = ...) to allow on.exit() to run on failure
-          
+
+          # Removed options(error = ...) to allow on.exit() to run on failure.
+
           library(here)
           setwd(here::here())
-          
+
           message("DEBUG: Child process WD = ", here::here())
-          
+
           dir.create(dirname(log_file), recursive = TRUE, showWarnings = FALSE)
-          
+
           con <- file(log_file, open = "wt")
           sink(con)
           sink(con, type = "message")
-          
+
           on.exit({
             sink(type = "message")
             sink()
             close(con)
           }, add = TRUE)
-          
+
           source(script_path, local = new.env(parent = globalenv()))
-          
+
         },
         args = list(script_path = script_path, log_file = log_file)
       )
@@ -41,7 +41,7 @@ run_script_project <- function(script_path, log_file) {
       cat("ERROR:", conditionMessage(e), "\n", file = log_file, append = TRUE)
     }
   )
-  
+
   message("=== END: ", log_file, " ===\n")
   invisible(NULL)
 }
